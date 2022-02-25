7c5d70d0b5754b81424811ed53842713816602c0
roeehtlvmc <roeehrl@gmail.com>
2022-02-25 13:45:44 +0200

    changes 25/2
    r

--- mysite/TestCatalog/views.py
+++ mysite/TestCatalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from .models import Test, Lab
 from django.views.generic import View
+from django.db.models.functions import Lower
 
  
 # Create your views here.
@@ -15,9 +16,9 @@ class searchView(View):
         template = "search.html"
         message = None
         if(letter != ""):
-            tests = Test.objects.filter(name__startswith=letter)
+            tests = Test.objects.filter(name__startswith=letter).order_by(Lower('name').asc()).distinct()
             if(letter == "all"):
-                tests = Test.objects.all()
+                tests = Test.objects.all().order_by(Lower('name').asc()).distinct()
         else:
             tests = None
 
@@ -26,7 +27,7 @@ class searchView(View):
         context = {
 
             "session" : "search",
-            "tests" : tests,
+            "tests" : tests
         }
     
         return render(request,template,context)
@@ -35,6 +36,10 @@ def searchViewText(request,text):
     tests = Test.objects.filter(name__icontains = text)
     try:
         tests |= Test.objects.filter(lab = Lab.objects.get(name = text))
+        tests |= Test.objects.filter(addnames__icontains = text)
+        tests |= Test.objects.filter(paneldetails__icontains = text)
+        tests |= Test.objects.filter(clinicalinfo__icontains = text)
+        tests = tests.distinct()
     except:
         pass
 
