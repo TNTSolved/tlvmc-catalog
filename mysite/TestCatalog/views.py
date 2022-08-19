2ec0a93871c53aca11c8632000bcdcbc38b2b2a0
roeehrl <82368940+roeehrl@users.noreply.github.com>
2022-08-19 19:57:49 +0000

    more ident

--- mysite/TestCatalog/views.py
+++ mysite/TestCatalog/views.py
@@ -14,71 +14,73 @@ import xlwt
 
 class Session:
     testss = Test.objects.all()
-    
+
     @classmethod
-    def displayedTest(self,ts = None):
-        if(ts != None):
+    def displayedTest(self, ts=None):
+        if (ts != None):
             Session.testss = ts
         return Session.testss
-      
-
 
 
 def getTestName(test):
-    if Test.objects.get(id = test.id).name:
-        return Test.objects.get(id = test.id).name
+    if Test.objects.get(id=test.id).name:
+        return Test.objects.get(id=test.id).name
     else:
         return ""
 
 
 def getdistinct(tests):
-        tests = tests
-        panel = ''
-        divided = {}
-        empties = list()
-        for t in tests:
-            if t.testcode != "" and t.testcode != "הבדיקה אינה ממוחשבת" and t.testcode != None:
-                if(panel != t.testcode):
-                    panel = t.testcode
-                    divided[panel] =  list()
-                    divided[panel].append(t)
-                if(panel == t.testcode):
-                    divided[panel].append(t)
-            else:
-             empties.append(t)
-        
-        unified = list()
-        for p in divided:
-            unified.append(divided[p][0])
-            
-        for p in empties:
-            unified.append(p)
-        
-        unified.sort(key = getTestName)
-        
-        return unified
+    tests = tests
+    panel = ''
+    divided = {}
+    empties = list()
+    for t in tests:
+        if t.testcode != "" and t.testcode != "הבדיקה אינה ממוחשבת" and t.testcode != None:
+            if (panel != t.testcode):
+                panel = t.testcode
+                divided[panel] = list()
+                divided[panel].append(t)
+            if (panel == t.testcode):
+                divided[panel].append(t)
+        else:
+            empties.append(t)
+
+    unified = list()
+    for p in divided:
+        unified.append(divided[p][0])
+
+    for p in empties:
+        unified.append(p)
+
+    unified.sort(key=getTestName)
+
+    return unified
 
 
 # Create your views here.
-def LabView(request,*args, **kwargs):
+def LabView(request, *args, **kwargs):
     link = kwargs['link']
     return redirect()
+
 
 def export_tests_csv(request):
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="tests.csv"'
 
     writer = csv.writer(response)
-    writer.writerow(['מס׳','קוד בדיקה','שם בדיקה','שמות נוספים','פירוט בדיקות המשך','הכנת החולה לפני הדיגום','סוג הדגימה','מגבלות הבדיקה','כלי קיבול לדגימה ','צבע פקק ','נפח דגימה מינימלי נדרש','נפח דם נדרש למקבצי בדיקות שונים','תנאי לקיחה ושימור טרם שינוע','תנאי שינוע','זמן מירבי מדיגום עד הגעה למעבדה','מען למשלוח בדיקות','מטרת הבדיקה','מידע קליני','שיטת ביצוע הבדיקה','משך הזמן מקבלת הדגימה ועד הפצת תשובה ','האם הבדיקה מבוצעת גם בערבים וסופ"ש? ','בקרת איכות חיצונית','קוד מחיר משרד הבריאות','הערות','מעבדה מבצעת'])
+    writer.writerow(['מס׳', 'קוד בדיקה', 'שם בדיקה', 'שמות נוספים', 'פירוט בדיקות המשך', 'הכנת החולה לפני הדיגום', 'סוג הדגימה', 'מגבלות הבדיקה', 'כלי קיבול לדגימה ', 'צבע פקק ', 'נפח דגימה מינימלי נדרש', 'נפח דם נדרש למקבצי בדיקות שונים', 'תנאי לקיחה ושימור טרם שינוע',
+                    'תנאי שינוע', 'זמן מירבי מדיגום עד הגעה למעבדה', 'מען למשלוח בדיקות', 'מטרת הבדיקה', 'מידע קליני', 'שיטת ביצוע הבדיקה', 'משך הזמן מקבלת הדגימה ועד הפצת תשובה ', 'האם הבדיקה מבוצעת גם בערבים וסופ"ש? ', 'בקרת איכות חיצונית', 'קוד מחיר משרד הבריאות', 'הערות', 'מעבדה מבצעת'])
 
     x = Session.displayedTest()
-    tests = x.values_list('id','testcode', 'name', 'addnames','posttests','preparment','kind','limitations','vessel','color','volume','bloodamount','pretransportconditions','transportconditions','maxtime','addressee','testpurpose','clinicalinfo','method','processingtime','specialdays','outtermonitoring','pricecode','comments')
+    tests = x.values_list('id', 'testcode', 'name', 'addnames', 'posttests', 'preparment', 'kind', 'limitations', 'vessel', 'color', 'volume', 'bloodamount', 'pretransportconditions',
+                          'transportconditions', 'maxtime', 'addressee', 'testpurpose', 'clinicalinfo', 'method', 'processingtime', 'specialdays', 'outtermonitoring', 'pricecode', 'comments')
 
     for test in tests:
-        assigner = list(test) 
-        assigner.append(x.get(id = test[0]).lab)
+        assigner = list(test)
+        assigner.append(x.get(id=test[0]).lab)
         tuple(assigner)
         writer.writerow(assigner)
+
 
 def export_tests_xslx(request):
     response = HttpResponse(content_type='application/ms-excel')
@@ -93,8 +95,8 @@ def export_tests_xslx(request):
     font_style = xlwt.XFStyle()
     font_style.font.bold = True
 
-    columns = ['מס׳','קוד בדיקה','שם בדיקה','שמות נוספים','פירוט בדיקות המשך','הכנת החולה לפני הדיגום','סוג הדגימה','מגבלות הבדיקה','כלי קיבול לדגימה ','צבע פקק ','נפח דגימה מינימלי נדרש','נפח דם נדרש למקבצי בדיקות שונים','תנאי לקיחה ושימור טרם שינוע','תנאי שינוע','זמן מירבי מדיגום עד הגעה למעבדה','מען למשלוח בדיקות','מטרת הבדיקה','מידע קליני','שיטת ביצוע הבדיקה','משך הזמן מקבלת הדגימה ועד הפצת תשובה ','האם הבדיקה מבוצעת גם בערבים וסופ"ש? ','בקרת איכות חיצונית','קוד מחיר משרד הבריאות','הערות','מעבדה מבצעת']
-
+    columns = ['מס׳', 'קוד בדיקה', 'שם בדיקה', 'שמות נוספים', 'פירוט בדיקות המשך', 'הכנת החולה לפני הדיגום', 'סוג הדגימה', 'מגבלות הבדיקה', 'כלי קיבול לדגימה ', 'צבע פקק ', 'נפח דגימה מינימלי נדרש', 'נפח דם נדרש למקבצי בדיקות שונים', 'תנאי לקיחה ושימור טרם שינוע',
+               'תנאי שינוע', 'זמן מירבי מדיגום עד הגעה למעבדה', 'מען למשלוח בדיקות', 'מטרת הבדיקה', 'מידע קליני', 'שיטת ביצוע הבדיקה', 'משך הזמן מקבלת הדגימה ועד הפצת תשובה ', 'האם הבדיקה מבוצעת גם בערבים וסופ"ש? ', 'בקרת איכות חיצונית', 'קוד מחיר משרד הבריאות', 'הערות', 'מעבדה מבצעת']
 
     for col_num in range(len(columns)):
         ws.write(row_num, col_num, columns[col_num], font_style)
@@ -102,17 +104,17 @@ def export_tests_xslx(request):
     # Sheet body, remaining rows
     font_style = xlwt.XFStyle()
     x = Session.displayedTest()
-    tests = x.values_list('id','testcode', 'name', 'addnames','posttests','preparment','kind','limitations','vessel','color','volume','bloodamount','pretransportconditions','transportconditions','maxtime','addressee','testpurpose','clinicalinfo','method','processingtime','specialdays','outtermonitoring','pricecode','comments')
-    
+    tests = x.values_list('id', 'testcode', 'name', 'addnames', 'posttests', 'preparment', 'kind', 'limitations', 'vessel', 'color', 'volume', 'bloodamount', 'pretransportconditions',
+                          'transportconditions', 'maxtime', 'addressee', 'testpurpose', 'clinicalinfo', 'method', 'processingtime', 'specialdays', 'outtermonitoring', 'pricecode', 'comments')
+
     for test in tests:
         row_num += 1
 
         assigner = list(test)
         try:
-            labname = x.get(id = test[0]).lab.name
-        except: 
+            labname = x.get(id=test[0]).lab.name
+        except:
             labname = None
-       
 
         assigner.append(labname)
 
@@ -125,109 +127,100 @@ def export_tests_xslx(request):
     return response
 
 
-
-
 class searchView(View):
-    
-    def get(self,request,letter=""):
-        letter = urllib.parse.unquote(letter,'windows-1255')
+
+    def get(self, request, letter=""):
+        letter = urllib.parse.unquote(letter, 'windows-1255')
         template = "search.html"
         message = None
         labs = Lab.objects.all()
-        if(letter != ""):
-            tests = getdistinct(Test.objects.filter(name__startswith=encoding.smart_str(letter, encoding='utf-8', strings_only=False, errors='strict')))
-          
-            if(letter == "all"):
+        if (letter != ""):
+            tests = getdistinct(Test.objects.filter(name__startswith=encoding.smart_str(
+                letter, encoding='utf-8', strings_only=False, errors='strict')))
+
+            if (letter == "all"):
                 tests = getdistinct(Test.objects.all())
         else:
             tests = None
 
-       
         context = {
 
-            "session" : "search",
-            "tests" : tests,
-            "labs" : labs
+            "session": "search",
+            "tests": tests,
+            "labs": labs
         }
-  
-       
-        return render(request,template,context)
-    
-def searchViewText(request,text):
-    text = urllib.parse.unquote(text,'windows-1255')
 
-    tests = Test.objects.filter(name__icontains = text)
+        return render(request, template, context)
+
+
+def searchViewText(request, text):
+    text = urllib.parse.unquote(text, 'windows-1255')
+
+    tests = Test.objects.filter(name__icontains=text)
     labs = Lab.objects.all()
-    
-    try:
-        tests |= Test.objects.filter(lab = Lab.objects.get(name = text))
-    except:
-            pass
-    try:
-        tests |= Test.objects.filter(addnames__icontains = text)
-    except:
-            pass
-    try:
-        tests |= Test.objects.filter(paneldetails__icontains = text)
-    except:
-            pass
-    try:
-        tests |= Test.objects.filter(clinicalinfo__icontains = text)
-    except:
-            pass
 
+    try:
+        tests |= Test.objects.filter(lab=Lab.objects.get(name=text))
+    except:
+        pass
+    try:
+        tests |= Test.objects.filter(addnames__icontains=text)
+    except:
+        pass
+    try:
+        tests |= Test.objects.filter(paneldetails__icontains=text)
+    except:
+        pass
+    try:
+        tests |= Test.objects.filter(clinicalinfo__icontains=text)
+    except:
+        pass
 
     tests = getdistinct(tests.distinct())
 
+    context = {
 
-    context={
-
-        "session" : "search",
-        "tests" : tests,
-        "labs" : labs
+        "session": "search",
+        "tests": tests,
+        "labs": labs
     }
-    return render(request,"search.html",context)
+    return render(request, "search.html", context)
 
 
-def searchViewLab(request,lab):
+def searchViewLab(request, lab):
     labs = Lab.objects.all()
-    labname = Lab.objects.get(id = lab)
+    labname = Lab.objects.get(id=lab)
     tests = Test.objects.none
     try:
-        tests = Test.objects.filter(lab = Lab.objects.get(id = lab))
+        tests = Test.objects.filter(lab=Lab.objects.get(id=lab))
         print(tests)
         tests = getdistinct(tests.distinct())
     except:
         pass
 
-    context={"tests" : tests,
-    "labs" : labs,
-    "lab":labname}
+    context = {"tests": tests,
+               "labs": labs,
+               "lab": labname}
 
-    return render(request,"search.html",context)
+    return render(request, "search.html", context)
 
 
 class TestDetailView(View):
 
     template = "testView.html"
 
-    def get(self,request,id):
-        test1 = Test.objects.get(id = id)
-        if(test1.testcode !=  "הבדיקה אינה ממוחשבת" and test1.testcode != ""):
-            panel = Test.objects.filter(testcode = test1.testcode)
-            if(len(panel) == 1):
+    def get(self, request, id):
+        test1 = Test.objects.get(id=id)
+        if (test1.testcode != "הבדיקה אינה ממוחשבת" and test1.testcode != ""):
+            panel = Test.objects.filter(testcode=test1.testcode)
+            if (len(panel) == 1):
                 panel = None
         else:
             panel = None
-       
-            
-       
-        
-        
-       
-        context = {
-            "test" : test1,
-            "panel" : panel
-            }
 
-        return render(request,self.template,context)
+        context = {
+            "test": test1,
+            "panel": panel
+        }
+
+        return render(request, self.template, context)
